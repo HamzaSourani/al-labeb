414da46ef863e73a62f97f300d939debc909f394
HamzaSourani <hamzasourani98@gmail.com>
2024-03-31 21:36:13 +0400

    add autoPlay property to video element

--- src/components/ui/video/index.tsx
+++ src/components/ui/video/index.tsx
@@ -42,6 +42,7 @@ const Video = ({ src, disableNextButton, validation, onNext }: VideoProps) => {
           className="aspect-square"
           onEnded={handleVideoEnd}
           src={`/assets/videos/${src}.mp4`}
+          autoPlay
         />
       </div>
       <div className="grid  p-4 [grid-template-columns:auto_1fr]">
