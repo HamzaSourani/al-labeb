13e84b7340450d8d1b0e94b05aead822aed452a2
HamzaSourani <hamzasourani98@gmail.com>
2024-04-02 01:05:10 +0400

    change end flow page design

--- src/pages/end/index.tsx
+++ src/pages/end/index.tsx
@@ -1,14 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import Video from "@/components/ui/video";
-import { Link } from "react-router-dom";
 
 const EndPage = () => {
+  const navigate = useNavigate();
+  const handleNext = () => {
+    navigate("/");
+  };
   return (
     <div className=" flex  items-center justify-center">
       <div className="md:w-1/2 lg:w-1/3 ">
-        <Video src="30" />
-      </div>
-      <div>
-        <Link to={"/"}>العودة إلى القائمة للرئيسة</Link>
+        <Video src="30" onNext={handleNext} validation="unSet" />
       </div>
     </div>
   );
