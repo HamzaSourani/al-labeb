d55c31ca70e414617992c4cb7263709252708268
HamzaSourani <hamzasourani98@gmail.com>
2024-04-06 19:42:06 +0400

    change yes | no icons

--- src/pages/nationalNumber/unExistAccount/index.tsx
+++ src/pages/nationalNumber/unExistAccount/index.tsx
@@ -1,7 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import Video from "@/components/ui/video";
-import NoIcon from "@/components/svg/no";
-import YesIcon from "@/components/svg/yes";
 import { useUserInfoContext } from "@/hooks/usUserInfo";
 
 const ExistAccountPage = () => {
@@ -25,13 +23,21 @@ const ExistAccountPage = () => {
           onClick={handlePositiveCase}
           className="mr-2 rounded-lg  border border-primary bg-gray-200 p-2 px-4 text-center text-xl shadow  hover:bg-gray-300"
         >
-          <YesIcon className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10" />
+          <img
+            className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10"
+            src="/assets/images/yes.png"
+            alt="yes"
+          />
         </button>
         <button
           onClick={handleNegativeCase}
           className="mr-2 rounded-lg  border border-primary bg-gray-200 p-2 px-4 text-center text-xl shadow  hover:bg-gray-300"
         >
-          <NoIcon className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10" />
+          <img
+            className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10"
+            src="/assets/images/no.png"
+            alt="no"
+          />
         </button>
       </div>
     </div>
--- src/pages/services/openAccount/maritalStatus/main/index.tsx
+++ src/pages/services/openAccount/maritalStatus/main/index.tsx
@@ -1,8 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Video from "@/components/ui/video";
 import { useUserInfoContext } from "@/hooks/usUserInfo";
-import NoIcon from "@/components/svg/no";
-import YesIcon from "@/components/svg/yes";
 
 const MaritalStatusMainPage = () => {
   const navigate = useNavigate();
@@ -20,7 +18,7 @@ const MaritalStatusMainPage = () => {
   };
   return (
     <div className="flex  flex-col items-center  justify-center gap-y-4">
-      <div className="shadow-shadow rounded-lg border  border-secondary  shadow md:w-1/2 lg:w-1/3 ">
+      <div className="rounded-lg border border-secondary  shadow  shadow-shadow md:w-1/2 lg:w-1/3 ">
         <Video src="3.4" />
       </div>
       <div className="flex justify-center">
@@ -28,13 +26,21 @@ const MaritalStatusMainPage = () => {
           onClick={handleGoToSpouseFirstName}
           className="mr-2 rounded-lg  border border-primary bg-gray-200 p-2 px-4 text-center text-xl shadow  hover:bg-gray-300"
         >
-          <YesIcon className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10" />
+          <img
+            className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10"
+            src="/assets/images/yes.png"
+            alt="yes"
+          />
         </button>
         <button
           onClick={handleHasHouse}
           className="mr-2 rounded-lg  border border-primary bg-gray-200 p-2 px-4 text-center text-xl shadow  hover:bg-gray-300"
         >
-          <NoIcon className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10" />
+          <img
+            className="h-6 w-6 fill-primary md:h-8 md:w-8 lg:h-10 lg:w-10"
+            src="/assets/images/no.png"
+            alt="no"
+          />
         </button>
       </div>
     </div>
