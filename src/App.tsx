8b1cf80e69d3f4b4e7da7ed00ceaec82bea39fa3
HamzaSourani <hamzasourani98@gmail.com>
2024-04-08 00:42:17 +0400

    add  request full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useRef } from "react";
 import { Outlet } from "react-router-dom";
-import "./App.css";
 import UserInfoProvider from "./hooks/usUserInfo";
+import "./App.css";
+
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null!);
+
   return (
-    <div className="h-screen ">
+    <div
+      ref={containerRef}
+      onClick={() => containerRef.current.requestFullscreen()}
+      className=" h-screen "
+    >
       <img
         className="absolute -z-10 h-screen w-screen object-cover"
         src="/bg.jpg"
