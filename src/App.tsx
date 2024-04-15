85908eb39c55152c1ecab7f38e3bc9f127b83fc3
HamzaSourani <hamzasourani98@gmail.com>
2024-04-15 20:27:10 +0400

    add animation between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import { useRef } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import UserInfoProvider from "./hooks/usUserInfo";
 import "./App.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null!);
-
+  const location = useLocation();
+  const variant = {
+    hidden: {
+      x: "-100vw",
+      transition: {
+        ease: "easeInOut",
+      },
+    },
+    visible: {
+      x: "0",
+      transition: {
+        // delay: 1,
+        duration: 1,
+      },
+    },
+    // exit: {
+    //   x: "100vw",
+    //   transition: {
+    //     duration: 1,
+    //     ease: "easeInOut",
+    //   },
+    // },
+  };
   return (
-    <div
-      ref={containerRef}
-      // onClick={() => containerRef.current.requestFullscreen()}
-      className=" h-screen "
-    >
-      <img
-        className="absolute -z-10 h-screen w-screen object-cover"
-        src="/bg.jpg"
-        alt="background"
-      />
-      <UserInfoProvider>
-        <Outlet />
-      </UserInfoProvider>
-    </div>
+    <AnimatePresence>
+      <div
+        ref={containerRef}
+        // onClick={() => containerRef.current.requestFullscreen()}
+        className=" h-screen "
+      >
+        <img
+          className="absolute -z-10 h-screen w-screen object-cover"
+          src="/bg.jpg"
+          alt="background"
+        />
+        <UserInfoProvider>
+          <motion.div
+            key={location.key}
+            variants={variant}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <Outlet />
+          </motion.div>
+        </UserInfoProvider>
+      </div>
+    </AnimatePresence>
   );
 }
 
